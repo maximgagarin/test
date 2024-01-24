3e0d3bc075aec6d8737982562cd7326e53800132
maximgagarin <max.emercom@yandex.ru>
2024-01-24 21:36:45 +0300

    24.01.21.55

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-{{--                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
